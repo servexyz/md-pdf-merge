521311eda0c44c71ada00e7027fb5871b8284a4d
alechp <me@alechp.com>
2018-02-20 08:29:20 -0800

    Added pdf sep logic

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hi");
+const merge = require("easy-pdf-merge");
+const mdpdf = require("markdown-pdf");
+const path = require("path");
+const fs = require("fs-extra");
+const log = console.log;
+//paths
+
+function PDFGen(directory) {
+  fs
+    .readdirSync(directory)
+    .filter(file => {
+      log(`File: ${file}`);
+      //TODO: Replace with regex
+      return file.includes(".pdf") || file.includes(".PDF");
+    })
+    .map(pdf => {
+      log(`PDF: ${pdf}`);
+    });
+}
+
+const root = path.join(__dirname, "../tests/sandbox");
+log(`root: ${root}`);
+PDFGen(root);
